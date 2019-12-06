03fa380e29924c1f479d8cbe083bb52bd88e6e8c
dat <dathalongbay@gmail.com>
2019-12-06 20:54:56 +0700

    111

---
+++ day11/001/index8.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Hằng số</h1>
+    <pre>
+        define("tên hằng số", "giá trị");
+    </pre>
+
+    <?php
+    define("PI", 3.14);
+    echo PI;
+    ?>
+
+</body>
+</html>
