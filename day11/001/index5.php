d2dac44736b95a33009e8a90ba2be654a2939277
dat <dathalongbay@gmail.com>
2019-12-06 20:24:50 +0700

    111

---
+++ day11/001/index5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>In ra thẻ html kèm các thuộc tính của nó</h1>
+    
+    <?php
+    echo "<p><a href=\"http://zing.vn\" style=\"color:red\">Zing VN</a></p>";
+    ?>
+</body>
+</html>
