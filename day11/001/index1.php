d4f03c38bf9cf84ce56652caf03ea44924ef2028
dat <dathalongbay@gmail.com>
2019-12-06 20:00:08 +0700

    111

---
+++ day11/001/index1.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 12/6/2019
+ * Time: 7:45 PM
+ */
+
+
+// chú thích 1 dòng
+
+/*
+chú thích nhiều dòng 1
+chú thích nhiều dòng 2
+chú thích nhiều dòng 3
+*/
+
+// in ra màn hình
+// câu lệnh php phải kết thúc bằng dấu ;
+echo "chào php";
---
+++ day11/001/index2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Trộn mã php và html</h1>
+
+    <?php
+    // mã PHP sẽ viết trong đây
+    echo "trộn mã php và html vào chung 1 file";
+    ?>
+
+
+</body>
+</html>
