44f881d4648431afcc5fcf8c9a84cfa2e9d1f257
dat <dathalongbay@gmail.com>
2019-12-17 19:53:14 +0700

    sdfdsf

---
+++ day13/004/index7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+    $ngoaite = [];
+    $ngoaite["aud"] = ["name" => "AUST.DOLLAR", "mua" => 15657, "ban" => 16050];
+    $ngoaite["usd"] = ["name" => "US DOLLAR", "mua" => 20657, "ban" => 23050];
+    $ngoaite["cad"] = ["name" => "CANADIAN DOLLAR", "mua" => 345666, "ban" => 1634324];
+?>
+
+tạo ra 1 bảng html có 4 cột và 4 dòng
+các cột Mã | Tên | Mua vào | Bán ra
+
+
+</body>
+</html>
