f7824a9ea4676f6b6dc078b68bdfb0d3b84b7cfa
dat <dathalongbay@gmail.com>
2019-12-24 18:30:05 +0700

    ádxfcghfjn

---
+++ day16/001/oop.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Cú pháp kết nối đến CSDL
+ * có IP là 192.168.1.1
+ * có username là admin7
+ * có password là 911
+ * có tên cơ sở dữ liệu là : ju
+ * sử dựng cách kết nối hướng đối tượng
+ */
