5db940b38fe1c103b28faf5bd34c144995738ad0
dat <dathalongbay@gmail.com>
2019-12-31 19:44:06 +0700

    efdghjkl,./

--- day17/001/remove.php
+++ day17/001/remove.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 12/27/2019
- * Time: 8:23 PM
- */
+// Nạp file kết nối cơ sở dữ liệu
+include_once "connect.php";
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+
+if (isset($_POST["id"]) && ($_POST["id"] > 0)) {
+    $id = (int) $_POST["id"];
+
+    $sqlDelete = "DELETE FROM sinhvien WHERE id=$id";
+
+    echo "<br> SQL thuần : " . $sqlDelete;
+
+
+    $connectMysql->exec($sqlDelete);
+
+    echo "<br>xóa bản ghi thành công";
+    echo "<a href='index.php'>Quay về trang chủ</a>";
+
+
+} else {
+    echo "dữ liệu gửi đi không hợp lệ";
+    exit;
+} ?>
