88cb00d0d1f59b961e2a5548fc3b19d918448af6
dat <dathalongbay@gmail.com>
2020-02-25 20:49:22 +0700

    dsfdsf

--- day21/006static/index.php
+++ day21/006static/index.php
@@ -6,11 +6,12 @@ class Oto {
     public $ten;
 
     // khai báo thuộc tính tĩnh
-    //public static $gia = 200;
+    public static $gia = 200;
 
 
     public function gioithieu() {
         echo "<br> giới thiệu";
+        $this->ten;
     }
 
     /**
@@ -18,9 +19,18 @@ class Oto {
      */
     public static function giaban() {
         echo "<br> giá bán của ô tô";
+
+        /**
+         * chú ý trong phương thức static không được phép
+         * sử dụng $this để chỉ chính class
+         * mà phải dùng self thay thế
+         *
+         */
+        echo "<br>" . self::$gia;
+
     }
 
 }
 Oto::giaban();
-// gọi đến thuộc tính tĩnh
-//echo "<br>" . Oto::gia;
+// gọi đến thuộc tính tĩnh vẫn phải giữ $
+echo "<br>" . Oto::$gia;
