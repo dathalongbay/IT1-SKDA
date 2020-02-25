58ea5fae84d4a29005ac8afe705e2d79de090f44
dat <dathalongbay@gmail.com>
2020-02-25 20:41:20 +0700

    fdsfdsf

--- day21/006static/index.php
+++ day21/006static/index.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2/25/2020
- * Time: 8:01 PM
- */
+
+
+class Oto {
+
+    public $ten;
+
+    // khai báo thuộc tính tĩnh
+    //public static $gia = 200;
+
+
+    public function gioithieu() {
+        echo "<br> giới thiệu";
+    }
+
+    /**
+     * khai báo phương thức tĩnh
+     */
+    public static function giaban() {
+        echo "<br> giá bán của ô tô";
+    }
+
+}
+Oto::giaban();
+// gọi đến thuộc tính tĩnh
+//echo "<br>" . Oto::gia;
