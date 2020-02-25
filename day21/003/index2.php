ed429f872b32caf1e72c59a5a54c0b0e584da138
dat <dathalongbay@gmail.com>
2020-02-25 19:19:38 +0700

    dsfdsf

---
+++ day21/003/index2.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Dienthoaicodien {
+
+    public $ten;
+    public $mausac = "đen";
+
+    public function goiDien() {
+        echo "<br> Gọi điện";
+    }
+
+    public function ngheDien() {
+        echo "<br> Nghe điện";
+    }
+}
+
+class Smartphone {
+
+
+    public function guiEmail() {
+        echo "<br> guiEmail";
+    }
+
+    public function choiGame() {
+        echo "<br> choiGame";
+    }
+}
+
+$samsung = new Smartphone();
+echo "<br> màu của ss : " . $samsung->mausac;
+echo "<br> " . $samsung->goiDien();
+echo "<br> " . $samsung->ngheDien();
+echo "<br> " . $samsung->guiEmail();
